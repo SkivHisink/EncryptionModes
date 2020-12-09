fcdb48c038b50d63d45b35877e22e1cdc995f2b6
Skiv Hisink <skiv27rus@gmail.com>
2020-12-09 13:39:55 +1000

    main.cpp fix for smart ptrs

--- EncryptionModes/main.cpp
+++ EncryptionModes/main.cpp
@@ -1,6 +1,7 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
+#include <iostream>
 #include <string>
 #include "CryptInterface.h"
 #include "CipherBlockChaining.h"
@@ -24,34 +25,34 @@ int main()
 		std::function<char(char, char)> dec = decryption_produce;
 		std::string input = "Hello world!";
 		//CBC
-		CryptInterface crypt_interface(new CipherBlockChaining(enc, dec));
-		std::string encrypted_cbc=input;
+		CryptInterface crypt_interface(std::make_shared<CipherBlockChaining>(enc, dec));
+		std::string encrypted_cbc = input;
 		std::string decrypted_cbc = input;
-		if (crypt_interface.encpypt_data(input, encrypted_cbc))
+		if (crypt_interface.encpypt_data(input, encrypted_cbc)) {
 			crypt_interface.decrypt_data(encrypted_cbc, decrypted_cbc);
+		}
 		//ECB
-		crypt_interface.set_strategy(new ElectronicCodebook(enc, dec));
+		crypt_interface.set_strategy(std::make_shared<ElectronicCodebook>(enc, dec));
 		std::string encrypted_ecb = input;
 		std::string decrypted_ecb = input;
-		if (crypt_interface.encpypt_data(input, encrypted_ecb))
+		if (crypt_interface.encpypt_data(input, encrypted_ecb)) {
 			crypt_interface.decrypt_data(encrypted_ecb, decrypted_ecb);
+		}
 		//OFB
-		crypt_interface.set_strategy(new OutputFeedback(enc, dec));
+		crypt_interface.set_strategy(std::make_shared<OutputFeedback>(enc, dec));
 		std::string encrypted_ofb = input;
 		std::string decrypted_ofb = input;
-		if (crypt_interface.encpypt_data(input, encrypted_ofb))
+		if (crypt_interface.encpypt_data(input, encrypted_ofb)) {
 			crypt_interface.decrypt_data(encrypted_ofb, decrypted_ofb);
+		}
 		//Check results
-		if (input.compare(decrypted_cbc))
-		{
+		if (input.compare(decrypted_cbc)) {
 			std::cout << "Bad Cipher Block Chaining encryption" << std::endl;
 		}
-		if (input.compare(decrypted_ecb))
-		{
+		if (input.compare(decrypted_ecb)) {
 			std::cout << "Bad Electronic Codebook encryption" << std::endl;
 		}
-		if (input.compare(decrypted_ofb))
-		{
+		if (input.compare(decrypted_ofb)) {
 			std::cout << "Bad Output Feedback encryption" << std::endl;
 		}
 	}
